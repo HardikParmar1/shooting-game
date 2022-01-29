965f39c784b78109c1944defdda0e6bb8bae7875
Hardik <mrhd0024@gmail.com>
2022-01-29 20:47:44 +0530

    added info and sound

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import {useState, useEffect} from "react";
 import Button from "./button";
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import InfoIcon from '@mui/icons-material/Info';
+import gunShot from "./sounds/sound.mp3";
 function App() {
   const [p1Score , setp1Score]= useState(0);
   const [p2Score , setp2Score]= useState(0);
@@ -10,6 +16,23 @@ function App() {
   const [winner , setWinner] = useState();
   const [p1Health , setP1Health ] = useState(100);
   const [p2Health , setP2Health ] = useState(100);
+  const [playSound, setPlaySound] = useState({
+      sound:true,
+      showIcon:"mute"
+  });
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   function setScore(){
     setp1RandomPower(Math.random()*5+1);
@@ -30,6 +53,11 @@ function App() {
     }else{
       setRound("Game Over")
     }
+
+    if(playSound.sound === true){
+      var audio = new Audio(gunShot);
+      audio.play();
+    }
   }
 
   function reset(){
@@ -42,6 +70,7 @@ function App() {
     document.querySelector(".p1-health-bar").style.width = "100%";
     document.querySelector(".p2-health-bar").style.width = "100%";
   }
+
 
 useEffect(()=>{
   if(p1Score>=3){
@@ -56,6 +85,46 @@ useEffect(()=>{
   return (
     <>
       <div className="container">
+      {playSound.showIcon === "mute"? (
+        <div className="sound">
+        <VolumeUpIcon onClick={() =>{setPlaySound({sound:false , showIcon:"unMute"})}} style={{ color: "#C2FFF9" }} sx={{ fontSize: 34 }}/>
+        </div>):
+
+      (<div className="sound">
+        <VolumeMuteIcon onClick={() =>{setPlaySound({sound:true , showIcon:"mute"})}} style={{ color: "#71DFE7" }} sx={{ fontSize: 34 }} />
+      </div>)
+      }
+
+      <div className="about">
+      <InfoIcon onClick={handleClick} style={{ color: "#C2FFF9" }} sx={{ fontSize: 33 }}/>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+             vertical: 'top',
+             horizontal: 'center',
+          }}
+      >
+        <Typography sx={{ p: 2 }} >
+            <>
+            <li className="about-title">Game details</li>
+            <li className="about-text"> The game has 5 rounds in total.</li>
+            <li className="about-text"> Both players will have 100% health when the game starts.</li>
+            <li className="about-text"> When you start the game the player generates power which reduces the opponent's health.</li>
+            <li className="about-text"> The player has to win at least 3 rounds to win the game.</li>
+            <li className="about-text"> The player who wins 3 games will automatically be declared as the winner with no more further rounds.</li>
+            </>
+        </Typography>
+      </Popover>
+    </div>
+
       <h1>{round>5 || round === "Game Over"?"Game Over" :  (!winner?`Round ${round}`: "Congratulations")}</h1>
         <div className="game">
           <div className="player">
@@ -98,6 +167,7 @@ useEffect(()=>{
       }
       </div>
       <h2>{winner}</h2>
+
       </div >
   </>
 );
